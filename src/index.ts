6aa07e4640dca06e9de2fd7bc3ee6419fc878f7d
exac <tgmclennan@hotmail.com>
2018-08-01 03:47:53 -0700

    hangouts-bot initial

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import express from "express"
 import cors from "cors"
 import * as bodyParser from "body-parser"
+const HangoutsBot = require("hangouts-bot")
 
 const app = express()
 const jsonParser = bodyParser.json();
 const urlEncodedParser = bodyParser.urlencoded({ extended: true });
 const port: number = parseInt(process.env.PORT) || 5000
+const bot = new HangoutsBot('botconbfuscate@gmail.com', 'Canada92');
+
+bot.on('online', function () {
+    console.log('hangouts-bot: online')
+})
+
+bot.on('offline', function () {
+    console.log('hangouts-bot: offline')
+})
+
+bot.on('message', function (from:any, message:any) {
+    console.log('hangouts-bot:', from, message)
+})
+
 // temporary
 const form = `<html><head></head><body><form method='POST' action=''><input name='email'/><textarea name='message'></textarea><input type='submit'/></form></body></html>`
 
@@ -23,6 +38,7 @@ app.get('/', async function (req: any, res: any) {
 
 app.post('/', urlEncodedParser, jsonParser, async function (req: any, res: any) {
     console.log(req.body)
+    bot.sendMessage('tgmclennan@gmail.com', req.body)
     return res.send(req.body)
 })
 

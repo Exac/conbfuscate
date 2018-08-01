a61aaa12022a5cc8fb22737d296dfa641c97c684
exac <tgmclennan@hotmail.com>
2018-08-01 03:09:16 -0700

    use env.PORT for port

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ import * as bodyParser from "body-parser"
 const app = express()
 const jsonParser = bodyParser.json();
 const urlEncodedParser = bodyParser.urlencoded({ extended: true });
-
+const port: number = parseInt(process.env.PORT) || 80
 // temporary
 const form = `<html><head></head><body><form method='POST' action=''><input name='email'/><textarea name='message'></textarea><input type='submit'/></form></body></html>`
 
 app.use(cors())
 app.use(urlEncodedParser);
 
-app.use(function (req:any, res:any, next:any) {
+app.use(function (req: any, res: any, next: any) {
     res.removeHeader('X-Powered-By')
     next()
 })
@@ -21,12 +21,12 @@ app.get('/', async function (req: any, res: any) {
     return res.send(form)
 })
 
-app.post('/', urlEncodedParser, jsonParser, async function (req:any, res:any) {
+app.post('/', urlEncodedParser, jsonParser, async function (req: any, res: any) {
     console.log(req.body)
     return res.send(req.body)
 })
 
-const server = app.listen(80, 'localhost', function () {
+const server = app.listen(port, 'localhost', function () {
     console.log(`conbfuscate @ http://0.0.0.0:80/`)
 })
 

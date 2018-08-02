85aac220bc58e490b538d3a13ac7f40c06c27365
exac <tgmclennan@hotmail.com>
2018-08-02 01:19:15 -0700

    implemented nodemailer

--- README.md
+++ README.md
@@ -1,22 +1,114 @@
 # Conbfuscate
 
-## Running Locally
+Conbfuscate is a middle-man that hides your email from bots when you only have access to a front-end.
+## Usage
+Example exposing email address to the world:
+```html
+<a href="mailto:your-email@example.com">Send me a message.</a>
+```
+
+HTML example:
+```html
+<form method="post" action="https://example-server-1.herokuapp.com/message">
+    <input name="email"/>
+    <textarea name="message"></textarea>
+    <button>Submit</button>
+</form>
+```
+
+Programatically:
+```js
+let email = "john@example.com";
+let message = "Hello,\nwe need to talk😊";
+fetch(`https://example-server-1.herokuapp.com/message/${email}/${message}`);
+```
+Result:
+
+Subject|```msg from john@example.com```
+--:|---
+To|```your-email@example.com```
+From|```your-bot@gmail.com```
+Message|```Hello,```<br>```we need to talk😊```
+
+## Setup
+### 1. Setup a bot gmail account
+
+Create a gmail account for your bot.
+
+Gmail will block your bot's login attempt by default. Log into gmail as your bot and visit: ```https://myaccount.google.com/lesssecureapps``` to enable your bot.
+
+![](https://i.imgur.com/zB0NV2s.png)
+
+### 2. Setting up bot's server environment
 
 Make sure you have [Node.js](http://nodejs.org/) and the [Heroku CLI](https://cli.heroku.com/) installed.
+Create a ```.env``` file and add the following:
+```
+NM_TRANS_EMAIL=yourBotsGmail@gmail.com
+NM_TRANS_PASS=yourBotsGmailPassword
+NM_TARGET=yourEmail@gmail.com
+```
 
+If you want to use Heroku, log into Heroku and add each line:
 ```sh
-$ git clone git@github.com:heroku/node-js-getting-started.git # or clone your own fork
-$ cd node-js-getting-started
-$ npm install
-$ npm start
+$ heroku login
+$ heroku config:set NM_TRANS_EMAIL=yourBotsGmail@gmail.com
+$ heroku config:set NM_TRANS_PASS=yourBotsGmailPassword
+$ heroku config:set NM_TARGET=yourEmail@gmail.com
 ```
+#### Port
+If you need to use a custom port locally, you can set the ```PORT``` environmental variable. *Do not* set the port on Heroku, as Heroku passes your app a custom port each run. The ```.env``` file is never pushed to Heroku.
 
-Your app should now be running on [localhost:5000](http://localhost:5000/).
-
-## Deploying to Heroku
-
-```
+### 3. Deploying to Heroku
+#### First run
+```sh
+$ heroku login
 $ heroku create
 $ git push heroku master
 $ heroku open
 ```
+Your server should now be running, test it out! In case conbfuscate is updated, you will need to push updates:
+
+#### Push Updates
+```sh
+$ git push heroku master
+$ heroku open
+```
+
+
+
+## Security
+Conbfuscate provides extra security in that it does not expose an email address to client front-ends. A malicious actor could send the Heroku server repeated message requests that would result in your emails being flooded, and your heroku account being rate-limited or charged, in an amplification attack for example.
+
+## Caveats
+Free Heroku apps are down for maintainance 1 hour per week.
+
+## Contributing
+Pull requests are welcome, please submit a pull request first, then create an issue explaining your modifications. Kindly link your pull request in your Issue.
+
+### Development
+Install from GitHub:
+```sh
+$ git clone https://github.com/Exac/conbfuscate.git
+$ cd conbfuscate
+$ git fork
+```
+#### Running the app
+##### Locally
+```sh
+$ npm install
+$ npm dev
+```
+
+##### Heroku
+```sh
+$ heroku login
+$ heroku local web
+```
+
+##### Remotley
+```sh
+$ npm install
+$ npm run start
+```
+
--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ import SMTPTransport = require("nodemailer/lib/smtp-transport");
 const app = express();
 const jsonParser = bodyParser.json();
 const urlEncodedParser = bodyParser.urlencoded({ extended: true });
-const port: number = parseInt(process.env.PORT) || 5000;
+const port: number = parseInt(process.env.PORT) || 80;
 
 // temporary
-const form = `<html><head></head><body><form method='POST' action=''><input name='email'/><textarea name='message'></textarea><input type='submit'/></form></body></html>`;
+const form = `<html><head></head><body><form method='POST' action='/message'><input name='email'/><textarea name='message'></textarea><input type='submit'/></form></body></html>`;
 
-// node-mailer
+// node-mailer setup
 const recipient = process.env.NM_TARGET;
 const transporter: nodemailer.Transporter = nodemailer.createTransport({
     service: "gmail",
@@ -22,50 +22,67 @@ const transporter: nodemailer.Transporter = nodemailer.createTransport({
         "pass": process.env.NM_TRANS_PASS
     }
 });
-const message: { from: string, to: string, subject: string, text: string } = {
+const message: { from: string, to: string, subject: string, text: string, html: string } = {
     from: process.env.NM_TRANS_EMAIL,
     to: process.env.NM_TARGET,
     subject: "msg from ",
-    text: ""
+    text: "",
+    html: "",
 };
 
-// middleware
+/**
+ * Sends an email. We do not care to report success status to the sender, only log it.
+ * @param to Email address of message sender.
+ * @param message Body of email
+ */
+async function send(to: string, msg: string | object) {
+    if (typeof msg === "object") msg = JSON.stringify(msg);
+    message.subject += `${to}.`;
+    message.text += msg;
+    message.html += `<pre>${msg}</pre>`;
+    console.log(message);
+    await transporter.sendMail(message)
+        .then(res => {
+            console.log(`Email sent to ${to}. Message: ${msg}`);
+        })
+        .catch((err) => {
+            console.error(`Error: nodemailer failed to send message to ${to}. Details: `, err);
+        });
+}
+
+// Middleware
 app.use(cors());
 app.use(urlEncodedParser);
 
 app.use(function (req: any, res: any, next: any) {
-    res.removeHeader("X-Powered-By");
+    res.removeHeader("X-Powered-By"); // shrink attack surface
     next();
 });
+
+// Routes
 
 app.get("/", async function (req: any, res: any) {
     return res.send(form);
 });
 
 app.post("/", urlEncodedParser, jsonParser, async function (req: any, res: any) {
-    console.log(req.body);
-    return res.send(req.body);
+    return res.send("Beep boop, I'm a bot.");
 });
 
-app.get("/message", async function (req: any, res: any) {
-    console.log("GET /message");
-    const name = req.body.name;
-    message.subject += name;
-    message.text += req.body;
-    await transporter.sendMail(message)
-        .then(res => {
-            console.log(`Email sent to ${name}. Message: ${req.body}`);
-            return res.status(200);
-        })
-        .catch((err) => {
-            console.error(`Error: nodemailer failed to send message to ${name}. Details: `, err);
-            return res.status(500);
-        });
-        return res.status(200);
+app.all("/message", function (req: any, res: any) {
+    if (req.body.email && req.body.message) send(req.body.email, req.body.message);
+    if (req.body.name && req.body.message) send(req.body.name, req.body.message);
+    return res.status(200).send("");
 });
+
+app.all("/message/:name/:message", async function (req: any, res: any) {
+    send(req.params.name, req.params.message);
+    return res.status(200).send("");
+});
+
+// Start server
 
 const server = app.listen(port, function () {
-    // console.log(`env: `, process.env);
     console.log(`conbfuscate @ http://localhost:${port}/`);
 });
 
